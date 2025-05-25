9942908e595362c5ad3e287b09e0a82efc19b312
Samuel Oyenuga <psa8.sa@gmail.com>
2025-05-25 21:51:08 +0100

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,40 +16,40 @@ export default function HomePage() {
     >
       <Header />
       <main>
-        <div className="bg-[url('/assets/bg1.png')] bg-repeat ">
-          <div className="bg-dark-navy/90">
-            <HeroSection />
+        <div className="bg-[url('/assets/bg1.png')] bg-cover">
+          {/* <div> */}
+          <HeroSection />
 
-            <div className="relative flex rotate-[180deg] h-[60px] xs:h-[80px]">
-              {Array(
-                window.innerWidth > 1280
-                  ? 5
-                  : window.innerWidth > 1024
-                    ? 4
-                    : window.innerWidth > 768
-                      ? 3
-                      : window.innerWidth > 380
-                        ? 2
-                        : 1
-              )
-                .fill(null)
-                .map((_, index) => (
-                  <svg
-                    key={index}
-                    xmlns="http://www.w3.org/2000/svg"
-                    preserveAspectRatio="none"
-                    viewBox="0 0 1000 100"
-                  >
-                    <path
-                      d="M500 4c-125 0-125 96-250 96S125 4 0 4V0h1000v4c-125 0-125 96-250 96S625 4 500 4Z"
-                      fill="#FBFFF4"
-                    ></path>
-                  </svg>
-                ))}
-            </div>
-
-            <FeaturesSection />
+          <div className="relative flex rotate-[180deg] h-[60px] xs:h-[80px]">
+            {Array(
+              window.innerWidth > 1280
+                ? 5
+                : window.innerWidth > 1024
+                  ? 4
+                  : window.innerWidth > 768
+                    ? 3
+                    : window.innerWidth > 380
+                      ? 2
+                      : 1
+            )
+              .fill(null)
+              .map((_, index) => (
+                <svg
+                  key={index}
+                  xmlns="http://www.w3.org/2000/svg"
+                  preserveAspectRatio="none"
+                  viewBox="0 0 1000 100"
+                >
+                  <path
+                    d="M500 4c-125 0-125 96-250 96S125 4 0 4V0h1000v4c-125 0-125 96-250 96S625 4 500 4Z"
+                    fill="#FBFFF4"
+                  ></path>
+                </svg>
+              ))}
           </div>
+
+          <FeaturesSection />
+          {/* </div> */}
         </div>
       </main>
       {/* <Footer /> */}
