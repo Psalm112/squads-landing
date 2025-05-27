0a079cb00ac9e78b5b8c5af7f2f72c15fe64dcd6
Samuel Oyenuga <psa8.sa@gmail.com>
2025-05-27 21:13:03 +0100

    first commit

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -8,7 +8,7 @@ function Blog() {
     <section className="max-w-7xl mx-auto p-4 sm:p-6">
       <div className="text-center flex flex-col gap-y-3">
         <h3 className="text-white text-2xl sm:text-3xl lg:text-5xl font-bold leading-tight">
-          Latest from the{' '}
+          Latest from the&nbsp;
           <span className="font-cursive text-light-green">Gang</span>
         </h3>
         <p className="text-cream/80">Subscribe to get latest yap from Wadmin</p>
